d32725295e33b91b244b24c68c5e1fcd51265334
Lana Gomeniuk <anochina@hotmail.com>
2021-11-20 16:28:38 +0200

    project finished

--- setup/fetch.js
+++ setup/fetch.js
@@ -2,17 +2,15 @@ const url = "https://api.chucknorris.io/jokes/random";
 const btn = document.querySelector(".btn");
 const content = document.querySelector(".content");
 
-btn.addEventListener("click", async () => {
-  try {
-    const data = await fetch(url);
-    const response = await data.json();
-    displayData(response);
-  } catch (error) {
-    console.log(error);
-  }
+btn.addEventListener("click", () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((response) => displayData(response))
+    .catch((err) => console.log(err));
 });
 
 function displayData({ value: joke }) {
   //   const { value: joke } = data;
+
   content.textContent = joke;
 }
---
+++ setup/promisify-xhr.js
@@ -0,0 +1,29 @@
+btn.addEventListener("click", () => {
+  getData(url)
+    .then((response) => displayData(response))
+    .catch((err) => console.log(err));
+});
+
+function getData(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) return;
+      if (xhr.status === 200) {
+        resolve(xhr.responseText);
+      } else {
+        reject({
+          status: xhr.status,
+          text: xhr.statusText,
+        });
+      }
+    };
+  });
+}
+
+function displayData(data) {
+  const { value: joke } = JSON.parse(data);
+  content.textContent = joke;
+}
